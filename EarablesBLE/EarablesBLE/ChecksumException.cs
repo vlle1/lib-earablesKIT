1302085c19f218d1d4b945b7a8d025f46b4c35a3
Erwin <e.mueller91718@gmail.com>
2020-01-31 10:48:32 +0100

    Exception Klasse hinzugefügt

---
+++ EarablesBLE/EarablesBLE/ChecksumException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EarablesKIT.Models.Library
+{
+    class ChecksumException : Exception
+    {
+        public ChecksumException(string message): base(message)
+        {
+
+        }
+    }
+}
