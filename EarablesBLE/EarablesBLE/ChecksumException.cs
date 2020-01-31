37543283e01a84ff485711fc9ca3605a657d5eaf
Erwin <e.mueller91718@gmail.com>
2020-01-31 10:50:11 +0100

    Kommentar für die neue Exception ergänzt

--- EarablesBLE/EarablesBLE/ChecksumException.cs
+++ EarablesBLE/EarablesBLE/ChecksumException.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace EarablesKIT.Models.Library
 {
+    /// <summary>
+    /// This exception is thrown if the checksum is incorrect
+    /// </summary>
     class ChecksumException : Exception
     {
         public ChecksumException(string message): base(message)
